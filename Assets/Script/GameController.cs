2fe9da37338c4cfc1d86605a339585090ad2f2fe
Chatchai Saratakij <CSaratakij@gmail.com>
2017-08-22 22:10:53 +0700

    Added timer and make it countdown every turn.

--- Assets/Script/GameController.cs
+++ Assets/Script/GameController.cs
@@ -8,6 +8,7 @@ namespace SichuanDynasty
     {
         public const int MAX_PLAYER_SUPPORT = 2;
         public const int MAX_PHASE_PER_PLAYER = 2;
+        public const float MAX_TIME_PER_PHASE = 60.0f;
 
         public bool IsGameInit { get { return _isGameInit; } }
         public bool IsGameStart { get { return _isGameStart; } }
@@ -28,9 +29,14 @@ namespace SichuanDynasty
         bool _isGameStart;
         bool _isGameOver;
 
+        bool _isNextTurn;
+        bool _isHasWinner;
+
+        bool _isInitNextTurn;
 
         Phase _currentPhase;
         Player[] _players;
+        Timer _timer;
 
 
         public GameController()
@@ -39,6 +45,9 @@ namespace SichuanDynasty
             _isGameInit = false;
             _isGameStart = false;
             _isGameOver = true;
+            _isNextTurn = false;
+            _isHasWinner = false;
+            _isInitNextTurn = false;
             _currentPhase = Phase.None;
             _players = new Player[MAX_PLAYER_SUPPORT];
         }
@@ -59,6 +68,8 @@ namespace SichuanDynasty
         public void GameStart()
         {
             _isGameStart = true;
+            //Test only..
+            _isNextTurn = true;
         }
 
         public void GameOver()
@@ -66,18 +77,40 @@ namespace SichuanDynasty
             _isGameOver = true;
         }
 
+
         void Awake()
         {
             for (int i = 0; i < _players.Length; i++) {
                 _players[i] = gameObject.AddComponent(typeof(Player)) as Player;
             }
+            _timer = gameObject.AddComponent(typeof(Timer)) as Timer;
+            _timer.SetMaxTime(MAX_TIME_PER_PHASE);
         }
 
         void Update()
         {
             if (_isGameInit && _isGameStart && !_isGameOver) {
                 Debug.Log("Game is started..");
+
+                if (_isNextTurn) {
+                    _timer.StartCountDown();
+                    _isInitNextTurn = false;
+                    _isNextTurn = false;
+                }
+
+                if (!_isInitNextTurn && !_timer.IsStarted && !_isHasWinner) {
+                    Debug.Log("Begin next turn..");
+                    _isInitNextTurn = true;
+                    StartCoroutine("_NextTurn");
+                }
+                Debug.Log("Timer Left : " + _timer.TimeLeft);
             }
+        }
+
+        IEnumerator _NextTurn()
+        {
+            yield return new WaitForSeconds(2.0f);
+            _isNextTurn = true;
         }
     }
 }
---
+++ Assets/Script/View/TimerView.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace SichuanDynasty.UI
+{
+    public class TimerView : MonoBehaviour
+    {
+        [SerializeField]
+        GameController gameController;
+
+        [SerializeField]
+        Text txtTimer;
+
+
+        void Update()
+        {
+            if (gameController && txtTimer) {
+                if (gameController.IsGameInit && gameController.IsGameStart) {
+                    txtTimer.text = gameController.GetComponent<Timer>().TimeLeft.ToString();
+                }
+            }
+        }
+    }
+}
